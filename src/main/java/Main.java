874a9b5f647063a3df347efbe12d4b85246ae07e
Tomas Hrdlovics <>
2022-02-07 10:41:53 -0500

    foobar feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        printDate();
+    }
+
+    private static void printDate() {
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+        Date date = calendar.getTime();
+
+        System.out.println(String.format("Current date is: %s", date));
     }
 }
